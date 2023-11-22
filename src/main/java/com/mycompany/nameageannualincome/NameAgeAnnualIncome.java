3879f8fed14636a815487fe14aae67149dcd490d
orlandomarin <orlandomarin@orlandos-air.lan>
2023-11-21 22:19:14 -0500

    Name Age Annual Income program challenge

---
+++ src/main/java/com/mycompany/nameageannualincome/NameAgeAnnualIncome.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.nameageannualincome;
+
+/**
+ *
+ * @author orlandomarin
+ */
+public class NameAgeAnnualIncome {
+
+    public static void main(String[] args) 
+    {
+        // declare variables
+        String name = "Orlando Marin";
+        int age = 20;
+        double annualPay = 200000.00;
+        
+        // print these values to the screen
+        System.out.println("My name is " + name + ", my age is " + age 
+                + ", and I hope to earn " + annualPay + " per year.");
+    }
+}
